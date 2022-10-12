65bccaf764e247d4de9dae3ff3bc04dc83f5d155
Jacob Lewallen <jlewallen@gmail.com>
2022-10-11 18:08:03 -0700

    Add and start using clap to parse args.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::path::PathBuf;
+
+use tracing::{debug, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use tracing_tree::HierarchicalLayer;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    path: Option<PathBuf>,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Serve {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    Registry::default()
+        .with(EnvFilter::from_default_env())
+        .with(
+            HierarchicalLayer::new(2)
+                .with_targets(true)
+                .with_bracketed_fields(true),
+        )
+        .init();
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Serve {}) => {
+            info!("serving");
+        }
+        None => {}
+    }
 }
