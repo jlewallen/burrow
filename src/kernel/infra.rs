a21bd54690f12b6bbae41c910b9e513f70f0db66
Jacob Lewallen <jlewallen@gmail.com>
2022-10-19 09:16:36 -0700

    Reorder methods in trait.

--- src/kernel/infra.rs
+++ src/kernel/infra.rs
@@ -4,6 +4,16 @@ use std::{cell::RefCell, rc::Rc};
 pub trait Infrastructure: Debug + LoadEntities {
     fn ensure_entity(&self, entity_ref: &DynamicEntityRef) -> Result<DynamicEntityRef>;
 
+    fn ensure_optional_entity(
+        &self,
+        entity_ref: &Option<DynamicEntityRef>,
+    ) -> Result<Option<DynamicEntityRef>> {
+        match entity_ref {
+            Some(e) => Ok(Some(self.ensure_entity(e)?)),
+            None => Ok(None),
+        }
+    }
+
     fn find_item(&self, args: ActionArgs, item: &Item) -> Result<Option<Rc<RefCell<Entity>>>>;
 
     fn find_optional_item(
@@ -15,16 +25,6 @@ pub trait Infrastructure: Debug + LoadEntities {
             self.find_item(args, &item)
         } else {
             Ok(None)
-        }
-    }
-
-    fn ensure_optional_entity(
-        &self,
-        entity_ref: &Option<DynamicEntityRef>,
-    ) -> Result<Option<DynamicEntityRef>> {
-        match entity_ref {
-            Some(e) => Ok(Some(self.ensure_entity(e)?)),
-            None => Ok(None),
         }
     }
 }
