faa4e39bc1f97cead1e84df9a7d1ddf5bed61b88
Jacob Lewallen <jlewallen@gmail.com>
2023-03-15 09:19:48 -0700

    Starting to tinker with wasm integration.

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -3,12 +3,14 @@ mod internal;
 pub mod build;
 #[allow(clippy::module_inception)]
 pub mod domain;
+pub mod dynamic;
 pub mod finding;
 pub mod perform;
 pub mod session;
 
 pub use build::*;
 pub use domain::*;
+pub use dynamic::*;
 pub use finding::*;
 pub use session::*;
 
