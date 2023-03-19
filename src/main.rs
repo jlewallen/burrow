27c738c21e6c4dfda6d2cd63331120c580625784
Jacob Lewallen <jlewallen@gmail.com>
2023-03-19 03:50:53 -0700

    Allow passing eval text to subcommand.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use std::error::Error;
 use std::path::PathBuf;
 use tracing::*;
@@ -29,12 +29,20 @@ struct Cli {
     command: Option<Commands>,
 }
 
+#[derive(Debug, Args)]
+pub struct Eval {
+    #[arg(short, long, default_value = "jlewallen")]
+    username: String,
+    #[arg(short, long, default_value = "look")]
+    text: String,
+}
+
 #[derive(Subcommand)]
 enum Commands {
     Serve(serve::Command),
     Shell(shell::Command),
+    Eval(Eval),
     Hacking,
-    Eval,
 }
 
 fn get_rust_log() -> String {
@@ -70,42 +78,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some(Commands::Hacking) => Ok(hacking::execute_command()?),
         Some(Commands::Serve(cmd)) => Ok(serve::execute_command(cmd)?),
         Some(Commands::Shell(cmd)) => Ok(shell::execute_command(cmd)?),
-        Some(Commands::Eval) => {
+        Some(Commands::Eval(cmd)) => {
             use crate::text::Renderer;
 
             let renderer = Renderer::new()?;
             let storage_factory = storage::sqlite::Factory::new("world.sqlite3")?;
             let domain = domain::Domain::new(storage_factory, false);
+            let session = domain.open_session()?;
 
-            {
-                let session = domain.open_session()?;
-
-                for text in &[
-                    "look", /*, "hold rake", "drop", "hold rake", "drop rake"*/
-                ] {
-                    if let Some(reply) = session.evaluate_and_perform("jlewallen", text)? {
-                        // info!("reply `{}`", reply.to_json()?);
-                        let text = renderer.render(reply)?;
-                        println!("{}", text);
-                    }
+            for text in &[&cmd.text] {
+                if let Some(reply) = session.evaluate_and_perform(&cmd.username, text)? {
+                    let text = renderer.render(reply)?;
+                    println!("{}", text);
                 }
-
-                session.close(&DevNullNotifier::default())?
             }
 
-            {
-                let session = domain.open_session()?;
-
-                for text in &["look"] {
-                    if let Some(reply) = session.evaluate_and_perform("jlewallen", text)? {
-                        // info!("reply `{}`", reply.to_json()?);
-                        let text = renderer.render(reply)?;
-                        println!("{}", text);
-                    }
-                }
-
-                session.close(&DevNullNotifier::default())?
-            }
+            session.close(&DevNullNotifier::default())?;
 
             Ok(())
         }
