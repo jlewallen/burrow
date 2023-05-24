d4512aa6ac4d7aebbbd811e2b15333ef869a4553
Jacob Lewallen <jlewallen@gmail.com>
2023-05-23 18:43:13 -0700

    Faster tera loading.
    It seems that the globbing is a pretty signifiant blow, performance
    wise. Without it and doing something less fancy the time to build this
    dropped from 386ms to 4ms.

--- cli/src/text.rs
+++ cli/src/text.rs
@@ -12,7 +12,36 @@ pub struct Renderer {
 impl Renderer {
     pub fn new() -> Result<Self> {
         let started = Instant::now();
-        let tera = Tera::new("text/**/*")?;
+        // Doing the below is 2ms vs 350ms. It appears to be the globbing,
+        // beause when I rereate the globbing here in a very simple fashion
+        // things are slow again.
+        // let tera = Tera::new("text/**/*")?;
+        // let walker = globwalk::glob("text /**/*")?;
+        /*
+        for entry in walker.filter_map(std::result::Result::ok) {
+            let path = entry.into_path();
+            info!("{:?}", path.display())
+        }
+
+        tera.add_template_files(vec![
+            ("text/replies/areaObservation.txt", None::<String>),
+            ("text/replies/editor.txt", None::<String>),
+            ("text/replies/impossible.txt", None::<String>),
+            ("text/replies/notFound.txt", None::<String>),
+            ("text/replies/what.txt", None::<String>),
+            ("text/replies/done.txt", None::<String>),
+            ("text/replies/simpleReply.txt", None::<String>),
+        ])?;
+        */
+        let mut tera = Tera::default();
+        for directory in ["text/replies"] {
+            let files: Vec<_> = std::fs::read_dir(directory)?
+                .filter_map(std::result::Result::ok)
+                .map(|path| (path.path(), None::<String>))
+                .collect();
+            tera.add_template_files(files)?;
+        }
+        tera.build_inheritance_chains()?;
         let elapsed = Instant::now() - started;
         info!(?elapsed, "compiled");
 
@@ -28,14 +57,14 @@ impl Renderer {
                     let mut context = Context::new();
                     context.insert(key, &value);
 
-                    let path = format!("replies/{}.txt", key);
+                    let path = format!("text/replies/{}.txt", key);
                     let text = self.tera.render(&path, &context)?;
                     all.push_str(&text);
                 }
             }
             serde_json::Value::String(name) => {
                 let context = Context::new();
-                let path = format!("replies/{}.txt", name);
+                let path = format!("text/replies/{}.txt", name);
                 let text = self.tera.render(&path, &context)?;
                 all.push_str(&text);
             }
