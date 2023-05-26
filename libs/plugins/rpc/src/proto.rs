425672f04af92d6328135ab741da8f1a986fe327
Jacob Lewallen <jlewallen@gmail.com>
2023-05-26 08:46:13 -0700

    Protocol work.

--- libs/plugins/rpc/src/proto.rs
+++ libs/plugins/rpc/src/proto.rs
@@ -68,7 +68,13 @@ pub enum Query {
 #[derive(Serialize, Deserialize)]
 pub struct QueryMessage {
     session_key: SessionKey,
-    query: Query,
+    query: Option<Query>,
+}
+
+impl QueryMessage {
+    fn into_tuple(self) -> (SessionKey, Option<Query>) {
+        (self.session_key, self.query)
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -98,4 +104,135 @@ pub enum Payload {
 pub struct PayloadMessage {
     session_key: SessionKey,
     payload: Payload,
+}
+
+impl PayloadMessage {
+    fn into_tuple(self) -> (SessionKey, Payload) {
+        (self.session_key, self.payload)
+    }
+}
+
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[cfg(test)]
+mod tests {
+    use anyhow::Result;
+
+    use crate::proto::SessionKey;
+
+    use super::{Payload, PayloadMessage, Query, QueryMessage};
+
+    enum PluginTransition {
+        Direct(PluginState),
+    }
+
+    enum PluginState {
+        Uninitialized,
+    }
+
+    impl PluginState {
+        fn handle(&self, payload: Payload) -> PluginTransition {
+            match self {
+                PluginState::Uninitialized => match payload {
+                    Payload::Initialize => todo!(),
+                    Payload::Evaluate(_, _) => todo!(),
+                    Payload::Entity(_) => todo!(),
+                    Payload::Found(_) => todo!(),
+                    Payload::Permission(_) => todo!(),
+                    Payload::Hook(_) => todo!(),
+                },
+            }
+        }
+    }
+
+    struct PluginProtocol {
+        state: PluginState,
+    }
+
+    impl Default for PluginProtocol {
+        fn default() -> Self {
+            Self {
+                state: PluginState::Uninitialized,
+            }
+        }
+    }
+
+    impl PluginProtocol {
+        fn handle(&self, payload: PayloadMessage) -> Self {
+            let (session_key, payload) = payload.into_tuple();
+            Self {
+                state: match self.state.handle(payload) {
+                    PluginTransition::Direct(state) => state,
+                },
+            }
+        }
+    }
+
+    enum ServerTransition {
+        Direct(ServerState),
+    }
+
+    enum ServerState {
+        SendingInitialize,
+    }
+
+    impl ServerState {
+        fn handle(&self, query: Option<Query>) -> ServerTransition {
+            match self {
+                ServerState::SendingInitialize => match query {
+                    Some(Query::Complete) => todo!(),
+                    Some(Query::Update(_)) => todo!(),
+                    Some(Query::Raise(_)) => todo!(),
+                    Some(Query::Chain(_)) => todo!(),
+                    Some(Query::Reply(_)) => todo!(),
+                    Some(Query::Permission(_)) => todo!(),
+                    Some(Query::Lookup(_)) => todo!(),
+                    Some(Query::Find(_)) => todo!(),
+                    Some(Query::Try(_)) => todo!(),
+                    None => ServerTransition::Direct(ServerState::SendingInitialize),
+                },
+            }
+        }
+    }
+
+    struct ServerProtocol {
+        state: ServerState,
+    }
+
+    impl Default for ServerProtocol {
+        fn default() -> Self {
+            Self {
+                state: ServerState::SendingInitialize,
+            }
+        }
+    }
+
+    impl ServerProtocol {
+        fn handle(&self, query: QueryMessage) -> Self {
+            let (session_key, query) = query.into_tuple();
+            Self {
+                state: match self.state.handle(query) {
+                    ServerTransition::Direct(state) => state,
+                },
+            }
+        }
+    }
+
+    fn create() -> Result<(ServerProtocol, PluginProtocol)> {
+        Ok((ServerProtocol::default(), PluginProtocol::default()))
+    }
+
+    #[test]
+    fn test_initialize() -> Result<()> {
+        let (server, plugin) = create()?;
+
+        let session_key: SessionKey = "session-key".to_owned();
+
+        server.handle(QueryMessage {
+            session_key: session_key.clone(),
+            query: None,
+        });
+
+        Ok(())
+    }
 }
