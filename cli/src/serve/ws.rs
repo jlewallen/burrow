3a1e4248e9137c7a011360e4d9e6e9e2fac94748
Jacob Lewallen <jlewallen@gmail.com>
2023-08-05 07:55:32 -0700

    Shuffling things around in web socket server side.

--- cli/src/serve/ws.rs
+++ cli/src/serve/ws.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
-use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::Extension, response::IntoResponse};
 use axum_typed_websockets::{Message, WebSocket, WebSocketUpgrade};
-use futures::{sink::SinkExt, stream::StreamExt};
+use futures::{
+    sink::SinkExt,
+    stream::{SplitStream, StreamExt},
+};
 use serde::{Deserialize, Serialize};
 use std::{borrow::Borrow, rc::Rc, sync::Arc};
 use tokio::sync::broadcast;
@@ -14,9 +17,7 @@ use kernel::{
 };
 use replies::ToJson;
 
-use crate::serve::{
-    handlers::TokenClaims, jwt_auth::ErrorResponse, rpc::try_parse_action, ClientSession,
-};
+use crate::serve::{handlers::TokenClaims, rpc::try_parse_action, ClientSession};
 
 use super::AppState;
 
@@ -45,34 +46,10 @@ pub async fn ws_handler(
     ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
-fn evaluate_commands<T>(
-    session: Rc<Session>,
-    notifier: &T,
-    eval_as: EvaluateAs,
-    text: &str,
-) -> Result<Effect>
-where
-    T: Notifier,
-{
-    let effect: Effect = if let Some(effect) = session
-        .evaluate_and_perform_as(eval_as, text)
-        .expect("Evaluation error")
-    {
-        effect
-    } else {
-        SimpleReply::What.into()
-    };
-
-    session.close(notifier).expect("Error closing session");
-
-    Ok(effect)
-}
-
-async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: Arc<AppState>) {
-    let (mut sender, mut receiver) = stream.split();
-
-    let mut session: Option<ClientSession> = None;
-
+async fn wait_for_token(
+    receiver: &mut SplitStream<WebSocket<ServerMessage, ClientMessage>>,
+    state: &Arc<AppState>,
+) -> Result<ClientSession> {
     while let Some(Ok(m)) = receiver.next().await {
         match m {
             Message::Item(ClientMessage::Token { token }) => {
@@ -80,34 +57,13 @@ async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: A
                     &token,
                     &jsonwebtoken::DecodingKey::from_secret(state.env.jwt_secret.as_ref()),
                     &jsonwebtoken::Validation::default(),
-                )
-                .map_err(|_| {
-                    let json_error = ErrorResponse {
-                        status: "fail",
-                        message: "Invalid token".to_string(),
-                    };
-                    (StatusCode::UNAUTHORIZED, Json(json_error))
-                });
-                session = match claims {
-                    Ok(claims) => {
-                        info!("claims: {:?}", claims.claims.sub);
+                )?;
 
-                        let key = claims.claims.sub;
+                let key = claims.claims.sub;
 
-                        Some(
-                            state
-                                .try_start_session(&EntityKey::new(&key))
-                                .expect("Error starting session"),
-                        )
-                    }
-                    Err(e) => {
-                        warn!("{:?}", e);
-
-                        None
-                    }
-                };
-
-                break;
+                return Ok(state
+                    .try_start_session(&EntityKey::new(&key))
+                    .expect("Error starting session"));
             }
             m => {
                 info!("unexpected: {:?}", m);
@@ -115,34 +71,25 @@ async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: A
         }
     }
 
-    if let Some(session) = &session {
-        info!("welcome");
+    Err(anyhow::anyhow!("No token"))
+}
 
-        let _ = sender
-            .send(Message::Item(ServerMessage::Welcome {
-                self_key: session.key.to_string(),
-            }))
-            .await;
-    } else {
-        /* Do this after checking credentials, avoids sending this on spurious
-         * disconnections. */
-        /*
-        info!("bad credentials");
+async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = stream.split();
 
-        let _ = sender
-            .send(Message::Item(ServerMessage::Error(
-                "Sorry, there's a problem with your credentials.".to_string(),
-            )))
-            .await;
-        */
+    info!("authenticating");
+    let Ok(session) = wait_for_token(&mut receiver, &state).await else {
+            return;
+    };
 
-        return;
-    }
+    info!("welcome");
+    let _ = sender
+        .send(Message::Item(ServerMessage::Welcome {
+            self_key: session.key.to_string(),
+        }))
+        .await;
 
-    // Consider handing off to another method here.
-    let session = session.unwrap();
     let our_key = session.key.to_string();
-
     let (session_tx, _session_rx) = broadcast::channel::<ServerMessage>(10);
 
     // Forward global events to the client if they're the intended audience.
@@ -178,82 +125,86 @@ async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: A
         }
     });
 
-    let user_state = state.clone();
-    let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(Message::Item(message))) = receiver.next().await {
-            trace!("item {:?}", &message);
+    let mut recv_task = tokio::spawn({
+        let state = state.clone();
+        async move {
+            while let Some(Ok(Message::Item(message))) = receiver.next().await {
+                trace!("item {:?}", &message);
 
-            match message {
-                ClientMessage::Perform(value) => {
-                    let app_state: &AppState = user_state.borrow();
+                match message {
+                    ClientMessage::Perform(value) => {
+                        let app_state: &AppState = state.borrow();
 
-                    let handle: JoinHandle<Result<serde_json::Value>> =
-                        tokio::task::spawn_blocking({
-                            let domain = app_state.domain.clone();
-                            let notifier = app_state.notifier();
-                            let our_key = our_key.clone();
+                        let handle: JoinHandle<Result<serde_json::Value>> =
+                            tokio::task::spawn_blocking({
+                                let domain = app_state.domain.clone();
+                                let notifier = app_state.notifier();
+                                let our_key = our_key.clone();
 
-                            move || {
-                                let session = domain.open_session().expect("Error opening session");
-                                // TODO This could be a PerformAction
-                                let action: Rc<_> = try_parse_action(value)
-                                    .expect("try parse action failed")
-                                    .into();
-                                let living = session
-                                    .entry(&LookupBy::Key(&EntityKey::new(&our_key)))
-                                    .expect("Living lookup failed")
-                                    .expect("Living not found");
-                                let perform = Perform::Living {
-                                    living,
-                                    action: PerformAction::Instance(action),
-                                };
-                                trace!("perform {:?}", &perform.enum_name());
-                                let _session = session.set_session()?;
-                                let effect = session.perform(perform).expect("Perform failed");
-                                session.close(&notifier).expect("Error closing session");
-                                Ok(effect.to_tagged_json()?)
-                            }
-                        });
+                                move || {
+                                    let session =
+                                        domain.open_session().expect("Error opening session");
+                                    // TODO This could be a PerformAction
+                                    let action: Rc<_> = try_parse_action(value)
+                                        .expect("try parse action failed")
+                                        .into();
+                                    let living = session
+                                        .entry(&LookupBy::Key(&EntityKey::new(&our_key)))
+                                        .expect("Living lookup failed")
+                                        .expect("Living not found");
+                                    let perform = Perform::Living {
+                                        living,
+                                        action: PerformAction::Instance(action),
+                                    };
+                                    trace!("perform {:?}", &perform.enum_name());
+                                    let _session = session.set_session()?;
+                                    let effect = session.perform(perform).expect("Perform failed");
+                                    session.close(&notifier).expect("Error closing session");
+                                    Ok(effect.to_tagged_json()?)
+                                }
+                            });
 
-                    match handle.await {
-                        Ok(Ok(reply)) => session_tx
-                            .send(ServerMessage::Reply(reply))
-                            .expect("Error sending reply"),
-                        Ok(Err(e)) => todo!("{:?}", e),
-                        Err(e) => todo!("{:?}", e),
-                    };
+                        match handle.await {
+                            Ok(Ok(reply)) => session_tx
+                                .send(ServerMessage::Reply(reply))
+                                .expect("Error sending reply"),
+                            Ok(Err(e)) => todo!("{:?}", e),
+                            Err(e) => todo!("{:?}", e),
+                        };
+                    }
+                    ClientMessage::Evaluate(text) => {
+                        let app_state: &AppState = state.borrow();
+
+                        let handle: JoinHandle<Result<serde_json::Value>> =
+                            tokio::task::spawn_blocking({
+                                let domain = app_state.domain.clone();
+                                let notifier = app_state.notifier();
+                                let text = text.clone();
+                                let our_key = our_key.clone();
+
+                                move || {
+                                    let session =
+                                        domain.open_session().expect("Error opening session");
+                                    let effect = evaluate_commands(
+                                        session,
+                                        &notifier,
+                                        EvaluateAs::Key(&EntityKey::new(&our_key)),
+                                        &text,
+                                    )?;
+                                    Ok(effect.to_tagged_json()?)
+                                }
+                            });
+
+                        match handle.await {
+                            Ok(Ok(reply)) => session_tx
+                                .send(ServerMessage::Reply(reply))
+                                .expect("Error sending reply"),
+                            Ok(Err(e)) => todo!("{:?}", e),
+                            Err(e) => todo!("{:?}", e),
+                        };
+                    }
+                    _ => todo!(),
                 }
-                ClientMessage::Evaluate(text) => {
-                    let app_state: &AppState = user_state.borrow();
-
-                    let handle: JoinHandle<Result<serde_json::Value>> =
-                        tokio::task::spawn_blocking({
-                            let domain = app_state.domain.clone();
-                            let notifier = app_state.notifier();
-                            let text = text.clone();
-                            let our_key = our_key.clone();
-
-                            move || {
-                                let session = domain.open_session().expect("Error opening session");
-                                let effect = evaluate_commands(
-                                    session,
-                                    &notifier,
-                                    EvaluateAs::Key(&EntityKey::new(&our_key)),
-                                    &text,
-                                )?;
-                                Ok(effect.to_tagged_json()?)
-                            }
-                        });
-
-                    match handle.await {
-                        Ok(Ok(reply)) => session_tx
-                            .send(ServerMessage::Reply(reply))
-                            .expect("Error sending reply"),
-                        Ok(Err(e)) => todo!("{:?}", e),
-                        Err(e) => todo!("{:?}", e),
-                    };
-                }
-                _ => todo!(),
             }
         }
     });
@@ -276,4 +227,27 @@ async fn handle_socket(stream: WebSocket<ServerMessage, ClientMessage>, state: A
     // tab or something.
 
     state.remove_session(&session);
+}
+
+fn evaluate_commands<T>(
+    session: Rc<Session>,
+    notifier: &T,
+    eval_as: EvaluateAs,
+    text: &str,
+) -> Result<Effect>
+where
+    T: Notifier,
+{
+    let effect: Effect = if let Some(effect) = session
+        .evaluate_and_perform_as(eval_as, text)
+        .expect("Evaluation error")
+    {
+        effect
+    } else {
+        SimpleReply::What.into()
+    };
+
+    session.close(notifier).expect("Error closing session");
+
+    Ok(effect)
 }
