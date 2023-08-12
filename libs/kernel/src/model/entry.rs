0d94b7c8068eb549dce439b45968c5c24e103bef
Jacob Lewallen <jlewallen@gmail.com>
2023-08-12 16:40:34 -0700

    It begins. Separate modules for Entry implementation.

--- libs/kernel/src/model/entry.rs
+++ libs/kernel/src/model/entry.rs
@@ -23,13 +23,6 @@ pub trait EntryResolver {
     }
 }
 
-#[derive(Clone)]
-pub struct Entry {
-    key: EntityKey,
-    entity: EntityPtr,
-    debug: Option<String>,
-}
-
 fn make_debug_string(entity: &Entity) -> String {
     let name = entity.name();
     let gid = entity.gid();
@@ -42,127 +35,150 @@ fn make_debug_string(entity: &Entity) -> String {
     }
 }
 
-impl Entry {
-    pub fn new(entity: EntityPtr) -> Self {
-        let (key, debug) = {
+pub use old::*;
+
+#[allow(dead_code)]
+mod new {
+    use std::{cell::RefCell, rc::Rc};
+
+    pub use super::*;
+
+    pub type Entry = Rc<RefCell<Entity>>;
+}
+
+#[allow(dead_code)]
+mod old {
+    pub use super::*;
+
+    #[derive(Clone)]
+    pub struct Entry {
+        key: EntityKey,
+        entity: EntityPtr,
+        debug: Option<String>,
+    }
+
+    impl Entry {
+        pub fn new(entity: EntityPtr) -> Self {
+            let (key, debug) = {
+                let entity = entity.borrow();
+                let key = entity.key().clone();
+                let debug = Some(make_debug_string(&entity));
+                (key, debug)
+            };
+
+            Self { key, entity, debug }
+        }
+
+        pub fn new_from_entity(entity: Entity) -> Result<Self, DomainError> {
+            Ok(Self {
+                key: entity.key().clone(),
+                entity: EntityPtr::new(entity),
+                debug: None,
+            })
+        }
+
+        pub fn new_from_json(key: EntityKey, value: JsonValue) -> Result<Self, DomainError> {
+            Ok(Self {
+                key,
+                entity: EntityPtr::new(Entity::from_value(value)?),
+                debug: None,
+            })
+        }
+
+        pub fn key(&self) -> &EntityKey {
+            &self.key
+        }
+
+        pub fn entity(&self) -> &EntityPtr {
+            &self.entity
+        }
+
+        pub fn entity_ref(&self) -> EntityRef {
+            let entity = self.entity.borrow();
+            entity.entity_ref()
+        }
+
+        pub fn to_json_value(&self) -> Result<JsonValue, DomainError> {
+            self.entity.borrow().to_json_value()
+        }
+
+        pub fn name(&self) -> Result<Option<String>, DomainError> {
+            let entity = self.entity();
             let entity = entity.borrow();
-            let key = entity.key().clone();
-            let debug = Some(make_debug_string(&entity));
-            (key, debug)
-        };
 
-        Self { key, entity, debug }
+            Ok(entity.name())
+        }
+
+        pub fn desc(&self) -> Result<Option<String>, DomainError> {
+            let entity = self.entity();
+            let entity = entity.borrow();
+
+            Ok(entity.desc())
+        }
+
+        pub fn scope_mut<T: Scope + Serialize>(
+            &self,
+        ) -> Result<OpenedScopeRefMut<T, Entity>, DomainError> {
+            let entity = self.entity();
+            Ok(entity.scope_mut::<T>()?)
+        }
     }
 
-    pub fn new_from_entity(entity: Entity) -> Result<Self, DomainError> {
-        Ok(Self {
-            key: entity.key().clone(),
-            entity: EntityPtr::new(entity),
-            debug: None,
-        })
+    impl OpenScope<Entry> for Entry {
+        fn scope<T: Scope>(&self) -> Result<Option<OpenedScope<T>>, DomainError> {
+            let entity = self.entity.borrow();
+            entity.scope::<T>()
+        }
     }
 
-    pub fn new_from_json(key: EntityKey, value: JsonValue) -> Result<Self, DomainError> {
-        Ok(Self {
-            key,
-            entity: EntityPtr::new(Entity::from_value(value)?),
-            debug: None,
-        })
+    impl Serialize for Entry {
+        fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            let name = self
+                .name()
+                .map_err(serde::ser::Error::custom)?
+                .unwrap_or_else(|| "None".to_owned());
+            let mut state = serializer.serialize_struct("Entry", 2)?;
+            state.serialize_field("key", &self.key)?;
+            state.serialize_field("name", &name)?;
+            state.end()
+        }
     }
 
-    pub fn key(&self) -> &EntityKey {
-        &self.key
+    impl TryFrom<EntityPtr> for Entry {
+        type Error = DomainError;
+
+        fn try_from(value: EntityPtr) -> Result<Self, Self::Error> {
+            Ok(Self::new(value))
+        }
     }
 
-    pub fn entity(&self) -> &EntityPtr {
-        &self.entity
+    impl TryFrom<&Entry> for EntityRef {
+        type Error = DomainError;
+
+        fn try_from(entry: &Entry) -> Result<Self, Self::Error> {
+            Ok(EntityRef::new_from_raw(&entry.entity().0))
+        }
     }
 
-    pub fn entity_ref(&self) -> EntityRef {
-        let entity = self.entity.borrow();
-        entity.entity_ref()
+    impl TryFrom<EntityRef> for Option<Entry> {
+        type Error = DomainError;
+
+        fn try_from(value: EntityRef) -> Result<Self, Self::Error> {
+            let session = get_my_session().expect("No active session");
+            session.entry(&LookupBy::Key(value.key()))
+        }
     }
 
-    pub fn to_json_value(&self) -> Result<JsonValue, DomainError> {
-        self.entity.borrow().to_json_value()
-    }
-
-    pub fn name(&self) -> Result<Option<String>, DomainError> {
-        let entity = self.entity();
-        let entity = entity.borrow();
-
-        Ok(entity.name())
-    }
-
-    pub fn desc(&self) -> Result<Option<String>, DomainError> {
-        let entity = self.entity();
-        let entity = entity.borrow();
-
-        Ok(entity.desc())
-    }
-
-    pub fn scope_mut<T: Scope + Serialize>(
-        &self,
-    ) -> Result<OpenedScopeRefMut<T, Entity>, DomainError> {
-        let entity = self.entity();
-        Ok(entity.scope_mut::<T>()?)
-    }
-}
-
-impl OpenScope<Entry> for Entry {
-    fn scope<T: Scope>(&self) -> Result<Option<OpenedScope<T>>, DomainError> {
-        let entity = self.entity.borrow();
-        entity.scope::<T>()
-    }
-}
-
-impl Serialize for Entry {
-    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let name = self
-            .name()
-            .map_err(serde::ser::Error::custom)?
-            .unwrap_or_else(|| "None".to_owned());
-        let mut state = serializer.serialize_struct("Entry", 2)?;
-        state.serialize_field("key", &self.key)?;
-        state.serialize_field("name", &name)?;
-        state.end()
-    }
-}
-
-impl TryFrom<EntityPtr> for Entry {
-    type Error = DomainError;
-
-    fn try_from(value: EntityPtr) -> Result<Self, Self::Error> {
-        Ok(Self::new(value))
-    }
-}
-
-impl TryFrom<&Entry> for EntityRef {
-    type Error = DomainError;
-
-    fn try_from(entry: &Entry) -> Result<Self, Self::Error> {
-        Ok(EntityRef::new_from_raw(&entry.entity().0))
-    }
-}
-
-impl TryFrom<EntityRef> for Option<Entry> {
-    type Error = DomainError;
-
-    fn try_from(value: EntityRef) -> Result<Self, Self::Error> {
-        let session = get_my_session().expect("No active session");
-        session.entry(&LookupBy::Key(value.key()))
-    }
-}
-
-impl std::fmt::Debug for Entry {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(debug) = &self.debug {
-            f.write_str(debug)
-        } else {
-            f.debug_struct("Entry").field("key", &self.key).finish()
+    impl std::fmt::Debug for Entry {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            if let Some(debug) = &self.debug {
+                f.write_str(debug)
+            } else {
+                f.debug_struct("Entry").field("key", &self.key).finish()
+            }
         }
     }
 }
